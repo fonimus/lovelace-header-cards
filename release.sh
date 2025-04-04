aa3f66887a80a3e7803bee33ffaf5ea3455e6d20
François Onimus <francois.onimus@gmail.com>
2025-04-04 14:13:52 +0200

    fix: release script

--- release.sh
+++ release.sh
@@ -1,4 +1,10 @@
 version=$1
 message=$2
 
-git add --all && git commit --message "fix: badge display" && git tag -a $version -m "$version" && git push origin $version && git push && gh release create $version --title "Release $version" --notes "$message" 'dist/*.js'
+npm run build
+git add --all
+git commit --message "fix: badge display"
+git tag -a $version -m "$version"
+git push origin $version
+git push
+gh release create $version --title "$version" --notes "$message" './dist/lovelace-header-cards.js'
